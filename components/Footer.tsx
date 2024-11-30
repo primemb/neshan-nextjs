b72a46570afcdc50ad74d87618ed5f8621bddce8
Mohammad Reza <mo.behzad34@gmail.com>
2024-11-30 15:29:02 +0330

    fix dark style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="w-full h-10 dark:bg-gray-800 bg-white flex items-center justify-center">
-      <p>
+    <footer className="w-full h-20 dark:bg-gray-800 bg-white flex items-center justify-center">
+      <p className="text-black dark:text-white">
         Developed with ❤️ by:{" "}
         <a href="https://mbehzadfar.dev" className="text-blue-500">
           MB
